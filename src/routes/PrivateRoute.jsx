d426f77fe37bed04b6941323deddefe460bcbd63
= <marcos.sousa462@gmail.com>
2024-09-24 13:17:05 -0300

    Criação do projeto

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom"
 
 export default function PrivateRoute({children}){
-  const isAuthenticated = localStorage.getItem('token') !== null
+  const isAuthenticated = localStorage.getItem('token') !== null && localStorage.getItem('name') !== null
   if(isAuthenticated){
     return children
   }
